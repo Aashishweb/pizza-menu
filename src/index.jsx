b2d2682060a230f17e5719478f699226060ba18c
unknown <ashishthakurfa@gmail.com>
2024-01-18 09:13:31 +0530

    styling components

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,11 @@ import ReactDom from "react-dom/client";
 
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>;
-      <Pizza />
+    <div className="container">
+      <h1>Hello React !!!!!!</h1>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
@@ -18,23 +20,35 @@ root.render(
   </React.StrictMode>
 );
 
-function Header(){
-
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>;
+    </header>
+  );
 }
 
-function Menu(){
-
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
 }
-
-function Footer(){
-  
+function Footer() {
+  return <footer>{new Date().toLocaleTimeString()}We're currently open</footer>;
+  // return React.createElement("footer", null, "We're currently open!");
 }
 
 function Pizza() {
   return (
     <div>
-      <img src="pizzas/funghi.jpg" alt="Pizza spinaci" />;
+      <img src="pizzas/funghi.jpg" alt="Pizza spinaci" />
       <h2>Pizza Spinaci</h2>
     </div>
-  )
+  );
 }
