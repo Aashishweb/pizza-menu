9c3963ed1cda22e211814c4f7c1914b4bd4b24da
unknown <ashishthakurfa@gmail.com>
2024-01-19 10:04:07 +0530

    done some styling and added props

--- src/index.jsx
+++ src/index.jsx
@@ -30,25 +30,41 @@ function Header() {
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
+      <Pizza
+        name="Pizza Spinachi"
+        ingredient="Tomoato, mozarel, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price="10"
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredient="Tomato, Mushrooms"
+        photoName="pizzas/funghi.jpg"
+        price="12"
+      />
+    </main>
   );
 }
 function Footer() {
-  return <footer>{new Date().toLocaleTimeString()}We're currently open</footer>;
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}We're currently open
+    </footer>
+  );
   // return React.createElement("footer", null, "We're currently open!");
 }
 
-function Pizza() {
+function Pizza(props) {
   return (
-    <div>
-      <img src="pizzas/funghi.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredient}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
